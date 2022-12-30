0ce3979c179d6a3341b168c0575ba5fae039944f
NatalieUSA <110269066+NatalieUSA@users.noreply.github.com>
2022-12-31 01:01:50 +0200

    create 01-color-switcher

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,43 @@
+const btnStart = document.querySelector('button[data-start]');
+const btnStop = document.querySelector('button[data-stop]');
 
+const TIMER_DELAY = 1000;
+let timerId = null;
+btnStop.disabled = true;
+
+btnStart.addEventListener('click', onStart);
+btnStop.addEventListener('click', onStop);
+
+createMarkup();
+
+function onStart(e) {
+  btnStart.disabled = true;
+  btnStop.disabled = false;
+
+  timerId = setInterval(() => {
+    document.body.style.backgroundColor = getRandomHexColor();
+  }, TIMER_DELAY);
+}
+
+function onStop(e) {
+  btnStart.disabled = false;
+  btnStop.disabled = true;
+  clearInterval(timerId);
+  createMarkup();
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+function createMarkup() {
+  btnStart.style.backgroundColor = getRandomHexColor();
+  btnStart.style.fontSize = '30px';
+  btnStart.style.border = 'none';
+  //btnStart.style.color = getRandomHexColor();
+
+  btnStop.style.backgroundColor = getRandomHexColor();
+  btnStop.style.border = 'none';
+  btnStop.style.fontSize = '30px';
+  //btnStop.style.color = getRandomHexColor();
+}
